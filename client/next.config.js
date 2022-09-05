d186d768aaeee692864a6884345ad8e608dabb10
Hunter Barton <axiysfire@gmail.com>
2022-09-05 17:31:53 -0500

    got it running

--- client/next.config.js
+++ client/next.config.js
@@ -1,22 +1,22 @@
-const isProd = process.env.NODE_ENV === 'production';
-const withPWA = require('next-pwa');
-const { runtimeCaching } = require('next-pwa/cache');
+// const isProd = process.env.NODE_ENV === 'production';
+// const withPWA = require('next-pwa');
+// const { runtimeCaching } = require('next-pwa/cache');
 
-/**
- * @type {import('next').NextConfig}
- **/
+// /**
+//  * @type {import('next').NextConfig}
+//  **/
 
-module.exports = withPWA({
-   images: {
-      loader: 'imgix',
-      path: 'https://mythosmystery.github.io'
-   },
-   pwa: {
-      dest: 'public',
-      runtimeCaching,
-      buildExcludes: [/middleware-manifest.json$/],
-      register: true,
-      skipWaiting: true,
-      disable: process.env.NODE_ENV != 'production'
-   }
-});
+// module.exports = withPWA({
+//    images: {
+//       loader: 'imgix',
+//       path: 'https://mythosmystery.github.io'
+//    },
+//    pwa: {
+//       dest: 'public',
+//       runtimeCaching,
+//       buildExcludes: [/middleware-manifest.json$/],
+//       register: true,
+//       skipWaiting: true,
+//       disable: process.env.NODE_ENV != 'production'
+//    }
+// });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,68 +1,67 @@
-import 'reflect-metadata';
+import 'reflect-metadata'
 
-import { ApolloServer } from 'apollo-server-express';
-import Express from 'express';
-import { buildSchema } from 'type-graphql';
-import { createConnection } from 'typeorm';
-import { __cookie__, __prod__ } from './consts';
+import { ApolloServer } from 'apollo-server-express'
+import Express from 'express'
+import { buildSchema } from 'type-graphql'
+import { createConnection } from 'typeorm'
+import { __cookie__, __prod__ } from './consts'
 
-import { MyContext } from './types/MyContext';
-import { HelloResolver } from './resolvers/Hello';
-import { DeleteNoteResolver } from './resolvers/note/Delete';
-import { GetNoteResolver } from './resolvers/note/Get';
-import { UpdateNoteResolver } from './resolvers/note/Update';
-import { WriteResolver } from './resolvers/note/Write';
-import { GetUserResolver } from './resolvers/user/Get';
-import { LoginResolver } from './resolvers/user/Login';
-import { MeResolver } from './resolvers/user/Me';
-import { RegisterResolver } from './resolvers/user/Register';
+import { MyContext } from './types/MyContext'
+import { HelloResolver } from './resolvers/Hello'
+import { DeleteNoteResolver } from './resolvers/note/Delete'
+import { GetNoteResolver } from './resolvers/note/Get'
+import { UpdateNoteResolver } from './resolvers/note/Update'
+import { WriteResolver } from './resolvers/note/Write'
+import { GetUserResolver } from './resolvers/user/Get'
+import { LoginResolver } from './resolvers/user/Login'
+import { MeResolver } from './resolvers/user/Me'
+import { RegisterResolver } from './resolvers/user/Register'
 
 const main = async () => {
-   if (__prod__) {
-      await createConnection({
-         type: 'postgres',
-         url: process.env.DATABASE_URL,
-         entities: ['dist/entity/*.*'],
-         ssl: { rejectUnauthorized: false },
-         synchronize: true
-      });
-   } else {
-      await createConnection();
-   }
+  if (__prod__) {
+    await createConnection({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: ['dist/entity/*.*'],
+      synchronize: true
+    })
+  } else {
+    await createConnection()
+  }
 
-   const schema = await buildSchema({
-      resolvers: [
-         HelloResolver,
-         GetUserResolver,
-         MeResolver,
-         RegisterResolver,
-         LoginResolver,
-         WriteResolver,
-         GetNoteResolver,
-         UpdateNoteResolver,
-         DeleteNoteResolver
-      ]
-   });
+  const schema = await buildSchema({
+    resolvers: [
+      HelloResolver,
+      GetUserResolver,
+      MeResolver,
+      RegisterResolver,
+      LoginResolver,
+      WriteResolver,
+      GetNoteResolver,
+      UpdateNoteResolver,
+      DeleteNoteResolver
+    ]
+  })
 
-   const app = Express();
+  const app = Express()
 
-   const apolloServer = new ApolloServer({
-      schema,
-      context: ({ req, res }: MyContext) => ({ req, res }),
-      playground: true,
-      introspection: true
-   });
+  const apolloServer = new ApolloServer({
+    schema,
+    context: ({ req, res }: MyContext) => ({ req, res }),
+    playground: true,
+    introspection: true
+  })
 
-   apolloServer.applyMiddleware({
-      app,
-      cors: {
-         origin: process.env.PROD_FRONTEND_URL
-      }
-   });
+  apolloServer.applyMiddleware({
+    app
+    // cors: {
+    //    origin: process.env.PROD_FRONTEND_URL
+    // }
+  })
 
-   app.listen(process.env.PORT || 3001, () => {
-      console.log(`server started on http://localhost:3001/graphql`);
-   });
-};
+  app.listen(process.env.PORT || 3001, () => {
+    console.log(`server started on http://localhost:3001/graphql`)
+  })
+}
 
-main();
+main()
